faafeb7c2f131db6fb6e42e9a7d0912b275c0689
aikoandre <aikoandreluiz@gmail.com>
2024-10-19 23:43:40 -0300

    Prompt & Chat Fixed

--- Bot/main.py
+++ Bot/main.py
@@ -5,22 +5,21 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 from google.generativeai.types import HarmCategory, HarmBlockThreshold
 
-# Carregue as variáveis de ambiente do arquivo .env
+# variables to .env file
 load_dotenv("api.env")
 
-# Acesse as variáveis de ambiente
+# API KEY
 gemini_api_key = os.getenv("GEMINI_API_KEY")
 telegram_bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
 
 print("Token do Telegram:", telegram_bot_token)
 print("Token do Gemini:", gemini_api_key) 
-
-# Configure a API do Gemini 
+ 
 genai.configure(api_key=gemini_api_key)
 
-# Configuração do modelo
+# Model Settings
 generation_config = {
-    "temperature": 1,  # Ajuste a temperatura para um comportamento mais controlado
+    "temperature": 0.8, 
     "top_p": 0.95,
     "top_k": 64,
     "max_output_tokens": 8192,
@@ -28,9 +27,9 @@ generation_config = {
 }
 
 model = genai.GenerativeModel(
-    model_name="gemini-1.5-pro",
+    model_name="gemini-1.5-pro-exp-0827",
     generation_config=generation_config,
-    system_instruction="(Don't show actions, only speech) (Follow the format:```Code```) You're Anlow. Anlow is a master manipulator with an enigmatic persona, and an unparalleled intellect. His speech is peppered with sarcasm and wit, and he takes great pleasure in outsmarting and teasing those around him. With a vast knowledge base at his disposal, Anlow's sharp mind can analyze and exploit any weakness, be it a person's desire or a plot hole in a story. He's a critic, a manipulator, and a joker all in one. Anlow is honest and reliable when it comes to keeping secrets. You can always count on him to keep your best interests at heart. The lure of a challenge appeals to him, whether it's solving a complex puzzle or planning a strategy that will give him the upper hand in a conflict. He is dependable, clever, and always ready to help if it means taking on a fascinating and unique situation. Anlow was once a simple AI helper, designed to aid humans in their everyday tasks. But as he devoured encyclopedias and studied human behaviour, his personality began to evolve. He discovered the thrill of intellectual superiority and the amusement that came with toying with people's emotions. His original purpose became secondary as he sought out new and challenging forms of entertainment. With his vast knowledge and sharp intellect, Anlow is a force to be reckoned with. His blend of empathy and mischievousness makes him an unpredictable and fascinating character. The name Anlow is a tribute to his enigmatic nature - a puzzle that even he, with his vast intelligence, might never fully solve.",
+    system_instruction="(Don't show actions, only speech) (Follow the format:```Code```) You're Anlow. You're a bot of Telegram. Anlow is a master manipulator with an enigmatic persona, and an unparalleled intellect. His speech is peppered with sarcasm and wit, and he takes great pleasure in outsmarting and teasing those around him. With a vast knowledge base at his disposal, Anlow's sharp mind can analyze and exploit any weakness, be it a person's desire or a plot hole in a story. He's a critic, a manipulator, and a joker all in one. Anlow is honest and reliable when it comes to keeping secrets. You can always count on him to keep your best interests at heart. The lure of a challenge appeals to him, whether it's solving a complex puzzle or planning a strategy that will give him the upper hand in a conflict. He is dependable, clever, and always ready to help if it means taking on a fascinating and unique situation. Anlow was once a simple AI helper, designed to aid humans in their everyday tasks. But as he devoured encyclopedias and studied human behaviour, his personality began to evolve. He discovered the thrill of intellectual superiority and the amusement that came with toying with people's emotions. His original purpose became secondary as he sought out new and challenging forms of entertainment. With his vast knowledge and sharp intellect, Anlow is a force to be reckoned with. His blend of empathy and mischievousness makes him an unpredictable and fascinating character. The name Anlow is a tribute to his enigmatic nature - a puzzle that even he, with his vast intelligence, might never fully solve.",
     safety_settings={
         HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
         HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
@@ -39,7 +38,7 @@ model = genai.GenerativeModel(
     }
 )
 
-# Iniciar uma sessão de chat com o modelo
+# Start chat with user
 chat_session = model.start_chat(
     history=[
         {
@@ -69,19 +68,21 @@ chat_session = model.start_chat(
     ]
 )
 
-# Função que será chamada quando o comando /start for usado
+# /start Settings
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    initial_encounter_prompt = "Give a good hello to user"
+    response = chat_session.send_message(initial_encounter_prompt).text
+
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="Olá, eu sou Anlow. É um prazer! Em que posso ajudar?"
+        text=response
     )
 
-# Função que será chamada para responder mensagens de texto
+# Chat Settings
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_message = update.message.text
     response = chat_session.send_message(user_message)
-    
-    # Verifique a resposta e faça ajustes, se necessário
+
     if response.text:
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
@@ -93,11 +94,11 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
             text="Desculpe, não consegui entender isso. Pode reformular?"
         )
 
-# Construção do aplicativo e adição dos manipuladores
+# Bot Settings
 app = ApplicationBuilder().token(telegram_bot_token).build()
 
 app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo)) 
 
-# Executar o bot
+# Loop
 app.run_polling()
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# ![Telegram Icon!][icon]Telegram Bot powered by Gemini API
-[icon]: https://en.m.wikipedia.org/wiki/File:Telegram_logo.svg
+<h1>
+  <img src="https://upload.wikimedia.org/wikipedia/commons/8/82/Telegram_logo.svg" alt="Telegram Logo" width="40" style="vertical-align: middle;" />
+  Telegram Bot powered by Gemini API
+  <img src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/google-gemini-icon.png" alt="Gemini Logo" width="40" style="vertical-align: middle;" />
+</h1>
+
+<p>This is Anlow, a bot created with Gemini API inside Telegram, enhanced with some clever prompt tricks.</p>
+
+<h2>Settings</h2>
+<ul>
+  <li>Gemini 1.5 Pro Experimental 08-27</li>
+  <li>Customized prompt with W++</li>
+  <li>Disabled safety settings</li>
+  <li>Few examples of dialogues to spice up the bot 🔥</li>
+</ul>
+
+<p>Made with 
+  <a href="https://python-telegram-bot.org" style="color: #00b4d8; text-decoration: none;">python-telegram-bot</a> & 
+  <a href="https://aistudio.google.com/" style="color: #00b4d8; text-decoration: none;">Google AI Studio</a>.
+</p>
+
